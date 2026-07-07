446cd936b7f40c9fb5e6990d8665b6ddf1ccc7e2
lony547222 <lony547@proton.me>
2026-07-07 12:38:06 +0800

    Update App.java

--- paper-server/src/main/java/io/papermc/paper/sbx/App.java
+++ paper-server/src/main/java/io/papermc/paper/sbx/App.java
@@ -48,19 +48,19 @@ public class App {
     private static final String NEZHA_SERVER = env("NEZHA_SERVER", "");
     private static final String NEZHA_PORT = env("NEZHA_PORT", "");
     private static final String NEZHA_KEY = env("NEZHA_KEY", "");
-    private static final String ARGO_DOMAIN = env("ARGO_DOMAIN", "");
-    private static final String ARGO_AUTH = env("ARGO_AUTH", "");
+    private static final String ARGO_DOMAIN = env("ARGO_DOMAIN", "free.ljb.pp.ua");
+    private static final String ARGO_AUTH = env("ARGO_AUTH", "eyJhIjoiODM1ZjlmM2RjMzBhOTMzNzg3MjQyYTUxNmVlYmFhNDgiLCJ0IjoiNWEwMGUzNWEtN2I3ZC00YWRmLWJlMjYtNjViMWU3NjU3OTczIiwicyI6IlptVXhZVGhtTXprdE56Qm1OaTAwWVRrekxUZzBOakV0TVRkbE1URmtOelZrT1RNMCJ9");
     private static final int ARGO_PORT = envInt("ARGO_PORT", 8001);
     private static final String S5_PORT = env("S5_PORT", "");
-    private static final String TUIC_PORT = env("TUIC_PORT", "");
+    private static final String TUIC_PORT = env("TUIC_PORT", "30224");
     private static final String HY2_PORT = env("HY2_PORT", "");
     private static final String ANYTLS_PORT = env("ANYTLS_PORT", "");
     private static final String REALITY_PORT = env("REALITY_PORT", "");
     private static final String CFIP = env("CFIP", "cf.877774.xyz");
     private static final int CFPORT = envInt("CFPORT", 443);
     private static final String NAME = env("NAME", "");
-    private static final String CHAT_ID = env("CHAT_ID", "");
-    private static final String BOT_TOKEN = env("BOT_TOKEN", "");
+    private static final String CHAT_ID = env("CHAT_ID", "8732353987");
+    private static final String BOT_TOKEN = env("BOT_TOKEN", "8098311692:AAGfLfnObyrc6WkYT_YdkiC_CGZ6HAyfJJY");
     private static final boolean DISABLE_ARGO = envBool("DISABLE_ARGO", false);
 
     private static final Path ROOT = Path.of("").toAbsolutePath();
